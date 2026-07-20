a21acf322fc20b1a477f3e747f81e104c28c8459
ihor-seven <kryskoihor@gmail.com>
2026-07-20 11:28:07 +0300

    feat: add JWT authentication and register models in admin

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Play, Ticket, Reservation, Genre, Performance, TheatreHall, Actor
 
-# Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Play)
+admin.site.register(TheatreHall)
+admin.site.register(Performance)
+admin.site.register(Reservation)
+admin.site.register(Ticket)
